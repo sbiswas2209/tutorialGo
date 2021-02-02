94ecda78bb3a8cde43af10358866bb71aa9d34a4
sbiswas2209 <sbiswas2209@outlook.com>
2021-02-02 09:45:03 +0530

    ✨ Configure SQLite

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sbiswas2209/tutorialGo
 
 go 1.15
 
-require github.com/gofiber/fiber v1.14.6
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/jinzhu/gorm v1.9.16
+)
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/sbiswas2209/tutorialGo/book"
+	"github.com/sbiswas2209/tutorialGo/database"
 )
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect!!!")
+	}
+	fmt.Println("Database Connection Successful")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+
+	fmt.Println("Database Migrated")
+
+}
 
 func helloWorld(c *fiber.Ctx) {
 	c.Send("Hello World")
@@ -18,6 +37,8 @@ func setupRoutes(a *fiber.App) {
 
 func main() {
 	app := fiber.New()
+	initDatabase()
+	defer database.DBConn.Close()
 	setupRoutes(app)
 	app.Listen(8080)
 }
